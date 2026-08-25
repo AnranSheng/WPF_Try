0d045fbfc94310d946183aeaf829f6a25a067b14
anran sheng <1984257202@qq.com>
2026-08-25 20:28:10 +0800

    初版备份

---
+++ WpfApp/Model/Login_User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp.Model
+{
+    internal class Login_User
+    {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
